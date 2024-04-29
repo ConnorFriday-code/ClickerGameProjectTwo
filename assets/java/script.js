f444d73664064158f9ba9329d005198fcbf003fd
Connor Friday <connor.friday.cf@gmail.com>
2024-04-29 03:40:05 +0000

    index.html: Changing the instructions screen cover's position from absolute to a fixed so it remains covering the screen even if the user scrolls. Adding a click function to remove the sceen cover when the user selects the 'start' button.

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+
     $("#instructions-outer-div").addClass("page-cover")
 
     let innerInstructions = $("<div></div>").addClass("instructions-outer-box");
 
-    let howToPlayh2 = $("<h2>How To Play!</h2>").addClass("how-to-play-title bg-yellow center");
+    let howToPlayh2 = $("<h2>How To Play!</h2>").addClass("how-to-play-title bg-yellow center oswald");
 
     let howToPlayDesc = $(`
     <div class="bg-grey how-to-play-desc">
@@ -19,14 +20,19 @@ $(document).ready(function(){
     `);
 
     let startGame = $(`
-    <div>
+    <div id="start-game">
         <p>Play</p>
     </div>
-    `).addClass("how-to-play-start bg-yellow center");
+    `).addClass("how-to-play-start bg-yellow center oswald");
 
     $("#instructions-outer-div").append(innerInstructions);
 
     $(".instructions-outer-box").append(howToPlayh2);
     $(".instructions-outer-box").append(howToPlayDesc);
     $(".instructions-outer-box").append(startGame);
+
+    $("#start-game").click(function(event){
+        $(".page-cover").hide("fast");
+    });
 })
+
