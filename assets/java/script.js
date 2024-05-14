62199e8e32b0463536020d0d57f1640e8f945ef6
Connor Friday <connor.friday.cf@gmail.com>
2024-05-14 02:32:38 +0000

    index.html, style.css, script.js: Updating all comments and adding additional ones missed.

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-
+    //Add class to make hidden, unfilled div into a page covering div
     $("#instructions-outer-div").addClass("page-cover border")
 
+    //Creat a variable that stores a div element with two classes
     let innerInstructions = $("<div></div>").addClass("instructions-outer-box border");
 
+    //Create a variable that stores a header 2 element with four classes
     let howToPlayh2 = $("<h2>How To Play!</h2>").addClass("how-to-play-title bg-yellow center oswald border");
 
+    ////Create a variable that stores all the instructions for the game
     let howToPlayDesc = $(`
     <div class="bg-grey how-to-play-desc border">
         <h3>What To Do</h3>
@@ -27,26 +30,32 @@ $(document).ready(function(){
     </div>
     `);
 
+    //Create a variable that stores a div with a paragraph that will serve as a button to start the game
     let startGame = $(`
     <div id="start-game">
         <p id="play-button">Play</p>
     </div>
     `).addClass("how-to-play-start bg-yellow center oswald border");
 
+    //Add the div meant to contain instruction content to div covering the screen
     $("#instructions-outer-div").append(innerInstructions);
 
+    //Add all data stored in variable above into the div directly above
     $(".instructions-outer-box").append(howToPlayh2);
     $(".instructions-outer-box").append(howToPlayDesc);
     $(".instructions-outer-box").append(startGame);
 
+    //On mouse entering start game button, turn border white
     $("#start-game").mouseenter(function(){
         $(this).css("border", "3px #fff solid"); 
     });
     
+    //On mouse leaving start game button, return border back to black
     $("#start-game").mouseleave(function(){
         $(this).css("border", "3px #000 solid");
     });
-    
+
+    //On start game div being clicked, make the instructions hide away
     $("#start-game").click(function(event){
         $(".page-cover").css("visibility","hidden");
         startUp();
@@ -85,12 +94,15 @@ function targetClicked(){
 
 //Point generator
 
+//Create variable called point and set it to zero
 let point=0;
 
+//On the target being clicked, run functions called scoreGenerate and newTarget
 $("#target").click(function(){
     scoreGenerate();
     newTarget();
 
+    //A function that calculates how much score should be added
     function scoreGenerate(){
 
         //If there are no upgrades, increase score by 1. If upgrades have been brought, increase score by 1 x the multiplier instead.
@@ -103,7 +115,7 @@ $("#target").click(function(){
             point+=1*upgradeMultiplier;
         }
 
-    
+        //Add point to score, change point to a string to be used in html
         $("#counter").text(point.toString());
     }
 
@@ -135,18 +147,23 @@ function newTarget(){
 
 //Upgrades
 
+//On mouse enter, the upgrade's border will turn white
 $(".upgrade-card").mouseenter(function(){
     $(this).css("border", "3px #fff solid")
 })
 
+//On mouse eave, the border whill return to black
 $(".upgrade-card").mouseleave(function(){
     $(this).css("border", "3px #000 solid")
 })
 
-
+//Create a upgrade multiplier and set it to zero
 let upgradeMultiplier = 0
 
+//Upgrade system
+//On upgrade 1 being clicked, check points
 $("#upgrade-1").click(function(){
+    //If points is equal to or greater than 9, minus 9 from score and increase upgrade multiplier by 10
     if(point>=9){
         //Minus 9 points
         point -=9;
@@ -156,6 +173,8 @@ $("#upgrade-1").click(function(){
 
         //Add 10 to score multiplier
         upgradeMultiplier +=10;
+
+    //If the points do equal or are greater than 9, then...
     } else{
         //Make the background turn red
         $("#upgrade-1").removeClass("bg-yellow").addClass("bg-red")
@@ -253,9 +272,14 @@ $("#upgrade-5").click(function(){
 
 //Game Over
 
+
+//On game over
 function gameOver(){
+    //Change game over dive from disabled to visible, then add class to cover screen
     $("#game-over-div").css("visibility","visible")
     $("#game-over-div").addClass("page-cover border");
+
+    //Create a variable containg game over content
     let gameOverInnerDiv = $(`
     <div id="game-over-outer-box" class="border">
         <h2 id="game-over-title" class="bg-red center oswald border">Game Over!</h2>
@@ -278,13 +302,17 @@ function gameOver(){
             <p id="restart-button-text" class="center bg-yellow border">Try Again?</p>
         </div>
     </div>`);
+
+    //Change html instead of adding html to game over div to prevent duplication
     $("#game-over-div").html(gameOverInnerDiv);
+
+    //On restart button being clicked, hide the game over div
     $("#restart-button").click(function(event){
         $(".page-cover").css("visibility","hidden");
         startUp();
     });
 
-    //Start timer function when user selects start game
+    //Restarts all stats and start timer on restart game being clicked
     $("#restart-button").click(function(){
         upgradeMultiplier = 0;
         point=0;
@@ -292,10 +320,12 @@ function gameOver(){
         timer();
     });
 
+    //On mouse entering restart button div, turn border white
     $("#restart-button-text").mouseenter(function(){
         $(this).css("border", "3px #fff solid");
     });
-    
+
+    //On mouse leaving restart button div, return border back to black
     $("#restart-button-text").mouseleave(function(){
         $(this).css("border", "3px #000 solid");
     });
